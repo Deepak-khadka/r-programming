4acec6ec9e6ff623b25fd50028aa133082db77fe
Deepak <rozankhdka159@gmail.com>
2022-08-19 08:35:28 +0545

    chore: include vector-coercion

---
+++ vector-coercion.R
@@ -0,0 +1,20 @@
+x <- c(1,"canada",3)
+
+class(x)
+
+#to convert number into character
+x <- 1:5
+
+y <- as.character(x)
+
+y
+
+#to change the character into numberic we use as.numeric
+
+#NA is for Not available
+
+x <- c("1", "b", "3")
+
+as.numeric(x)
+
+
